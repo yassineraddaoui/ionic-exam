ffeeb941ce56414cd075227207d40987cc219b9c
yassineraddaoui <yassineraddaouicontact@gmail.com>
2024-10-23 13:53:41 +0100

    Url params

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  isToastOpen = false;
 
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
+  list = [
+    {
+      name: 'Google',
+      value: 'google',
+      image: '	https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    },
+    {
+      name: 'Facebook',
+      value: 'facebook',
+      image: '	https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    },
+    {
+      name: 'Ionic',
+      value: 'ionic',
+      image: '	https://ionicframework.com/docs/img/demos/thumbnail.svg',
+    },
+  ];
   constructor() {}
-
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  id=this.routes.snapshot.paramMap.get('id');
+  
+  constructor(private routes:ActivatedRoute) {}
 
 }
